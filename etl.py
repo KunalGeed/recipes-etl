d19d5e191f19d2fca550175fc64035aa39aa4e27
Kunal Geed <kunalgeed15@gmail.com>
2024-01-22 20:36:26 +0100

    Added output_file.csv

--- etl.py
+++ etl.py
@@ -4,11 +4,43 @@ from fuzzywuzzy import fuzz
 import timeit
 import re
 from typing import Union
+import pandas as pd
 
+def read_dataset(filepath, lines=True):
+    """
+    Read the dataset from the specified file path. Added support for CSV.
+
+    Parameters:
+    - filepath (str): The path to the dataset file.
+
+    Returns:
+    - pd.DataFrame: The DataFrame containing the dataset.
+    """
+    try:
+        # Extract the file extension
+        file_extension = filepath.split('.')[-1].lower()
+
+        # Check the file type and use the appropriate loading function
+        if file_extension == 'json':
+            df = pd.read_json(filepath, lines=True)
+        elif file_extension in ['csv', 'txt']:
+            df = pd.read_csv(filepath)
+        else:
+            raise ValueError(f"Unsupported file format: {file_extension}")
+
+        return df
+    except FileNotFoundError:
+        print(f"Error: File not found at {filepath}")
+        return None
+    except ValueError as ve:
+        print("Value Error", ve)
+        return None
+    except Exception as e:
+        print(f"An error occurred while reading the dataset: {e}")
+        return None
+    
 def calculate_total_time(cookTime,prepTime) -> Union[int, None]:
     total_time=0
-    print(cookTime)
-    print(prepTime)
     if (not cookTime or cookTime=="PT") and (not prepTime or prepTime=="PT"):
         return None
     
@@ -99,6 +131,8 @@ if __name__ == "__main__":
     target_word="chillies"
     df_filtered=filter_dataframe_by_word(df,col_name, target_word)
     df_difficulty=add_difficulty(df_filtered)
+    df_difficulty.to_csv('output_file.csv', index=False)
+
     
 
 
